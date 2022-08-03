02d813a7e669fff836242fbc49adf55f9a187e7c
abrilo <abrilojson@gmail.com>
2022-08-03 07:06:39 -0400

    backdrop spinner added for sample

--- src/components/layouts/spinners/Backdrop.jsx
+++ src/components/layouts/spinners/Backdrop.jsx
@@ -1,12 +1,76 @@
-import React from 'react';
+import React from "react";
+import PropTypes from "prop-types";
+import Backdrop from "@mui/material/Backdrop";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
+import getDesignTokens from "../../../constants/theme";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { getLocalStorage } from "../../../utils/Storage";
 
-const Backdrop = () =>{
+const MainBackdrop = ({ title, subtitle, open }) => {
+  const [mode, setMode] = React.useState(
+    getLocalStorage("theme") ? getLocalStorage("theme") : "light"
+  );
+  const theme = React.useMemo(() => createTheme(getDesignTokens(mode)), [mode]);
 
-    return(
-        <>
-            <h1 style={{marginTop: '100px'}}> Spinner Loader</h1>
-        </>
-    )
+  React.useEffect(() => {
+    let themeMode = getLocalStorage("theme");
+    let t = themeMode ? themeMode : "light";
+    setMode(t);
+  }, [mode]);
+  
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <Backdrop
+          sx={{
+            color: "#218C6F",
+            zIndex: (theme) => theme.zIndex.drawer + 1,
+            display: "block",
+            backgroundColor: `background.paper`,
+          }}
+          open={true}
+        >
+          <Box
+            sx={{ p: { md: 2, sm: 0 }, height: `100%` }}
+          >
+            <Box sx={{ display: "flex" }}>
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "flex-start",
+                  ml: 1,
+                  cursor: "pointer",
+                  mt: 2,
+                }}
+              ></Box>
+              <Box sx={{ flexGrow: 1 }} />
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                textAlign: "center",
+                mt: 15,
+              }}
+            >
+              <CircularProgress
+                sx={{  position: "absolute", top: `35%` }}
+                size={110}
+                thickness={10}
+                color='secondary'
+              />
+            </Box>
+          </Box>
+        </Backdrop>
+      </ThemeProvider>
+    </>
+  );
+};
+MainBackdrop.propTypes = {
+  color: PropTypes.string,
+  open: PropTypes.bool,
+  appLayer: PropTypes.bool,
 };
 
-export default Backdrop;
+export default MainBackdrop;
